7533a359cdf22d61ccd593144afbefa79d14a289
khadeejadurrani <onebloom2024@gmail.com>
2025-06-26 21:26:29 +0000

    string conversion simple steps  and code

---
+++ 01_basics/02_stringmethods.js
@@ -0,0 +1,21 @@
+let str1 = "hello ";
+let str2 = " khadeeja";
+
+let str3= str1 + str2;
+console.log(str3);
+
+ console.log("1" + 2);
+ console.log(1 + "2");
+ console.log("1" + 2 + 2);
+ console.log(2 + 2 + "1");
+
+ console.log((3+4)*5%3); //always use brackets and dont merge the operations
+
+ console.log(true);
+ console.log(+true);
+
+ let num1 , num2 , num3;
+ num1 = num2 =num3 = 2+2;
+ let gameCounter =  100;
+ gameCounter++;
+ console.log(gameCounter);
