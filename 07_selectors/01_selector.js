ab86b5778dbebde3d48c85c3a9d38b87b4c22397
khadeejadurrani <onebloom2024@gmail.com>
2025-07-14 22:28:54 +0000

    selectors simple

---
+++ 07_selectors/01_selector.js
@@ -0,0 +1,4 @@
+/*writing this here for a context ,it is the shorter version of forEach loop in arrow function
+fruits.forEach(fruit => console.log(fruit)); */
+const fruits = document.querySelectorAll(".fruit");
+fruits.forEach(f => console.log(f.textContent) );
