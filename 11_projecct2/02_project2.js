039151431a17552c017e02ae5ec6e456d1acccad
khadeejadurrani <onebloom2024@gmail.com>
2025-07-19 21:35:58 +0000

    project2

--- 11_projecct2/02_project2.js
+++ 11_projecct2/02_project2.js
@@ -1,7 +1,18 @@
 const form = document.querySelector('form');
 
-form.addEventListener('submit' , function(e)){
+form.addEventListener('submit' , function(e){
     e.preventDefault();
 
-    parseInt(document.querySelector('#height').value);
-}
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#results')
+
+    if(height === '' ||  height < 0 || isNaN(height)){
+        results.innerHTML = `Please give me a valid height ${height}`;
+    }else if(weight === '' ||  weight < 0 || isNaN(weight)){
+        results.innerHTML = `Please give me a valid weight ${weight}`;
+    }else{
+        const bmi = (weight / ((height*height)/10000)).toFixed(2);
+        results.innerHTML = `<span>${bmi}</span>`;
+    }
+})
